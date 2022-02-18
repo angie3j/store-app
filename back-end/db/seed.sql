422ba4558f191bcd33175831ea7559b1e24ab6b1
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-18 14:10:51 -0500

    Create a Database & Seed

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,9 @@
-\c cta_dev;
+\c happy_sparky_boutique;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO products (name, reviews, image, description, price, size, color, featured) VALUES
+('Happy Sparky Dark Brown Leather Dog Leash', '4.4 out of 5', ' https://imgur.com/PurecVQ', 'Water Resistant Heavy Full Grain Leather Lead', '$59.99', 'Dark Brown', 'true'),
+('Happy Sparky Tan Leather Dog Leash', '4.4 out of 5', ' https://imgur.com/DiSse2a', 'Water Resistant Heavy Full Grain Leather Lead', '$49.99', 'Tan', 'false'),
+('Happy Sparky Leather Dog Leash', '', '', '', '', '')
+-- ('', '', '', '', '', '');
+-- ('', '', '', '', '', '');
+
