f9b82928d4693a87e7836d6558c7da616e8d8ed1
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-18 14:06:24 -0500

    Create Database & Seed

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,11 +1,20 @@
-DROP DATABASE IF EXISTS cta_dev;
-CREATE DATABASE cta_dev;
+DROP DATABASE IF EXISTS happy_sparky_boutique;
+CREATE DATABASE happy_sparky_boutique;
 
-\c cta_dev;
+\c happy_sparky_boutique;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS products;
 
-CREATE TABLE test (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    reviews TEXT,
+    image TEXT,
+    description TEXT, 
+    price TEXT,
+    size TEXT, 
+    color TEXT
+    featured BOOLEAN DEFAULT false
+    
 );
+
