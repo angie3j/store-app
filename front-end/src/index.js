443eb54de3bdd0ef0cb2acc60ca6e29ae4b2e170
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-19 18:27:38 -0500

    Updates 2

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-dom-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
