56fdd8921bde9d2e1b9f73450bde5bf63e238f70
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-19 23:31:26 -0500

    Create Path Routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,63 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-const API = process.env.REACT_APP_API_URL;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-console.log(API);
+// Pages
+import Home from './Pages/Home';
+// import Index from './Pages/Index';
+// import New from './Pages/New';
+// import Show from './Pages/Show';
+// import Reload from './Pages/Reload';
+
+// Components
+// import Product from './Components/Product';
+// import NavBar from './Components/NavBar';
+
+
+
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
   return (
     <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
+        {/* <NavBar /> */}
+        <Router>
+          <Routes>
+            <Route path='/' element={ <Home />}/>
+            {/* <Route path='/products' element={ <Product /> } /> */}
+            {/* <Route path='/products/new' element={ <NewForm />} /> */}
+            {/* <Route exact path='/products/:id' element={ <Show /> } /> */}
+            {/* <Route path='/produts/:id/edit' element={ <Edit /> } /> */}
+          </Routes>
+        </Router>
     </div>
-  );
+  )
 }
+
+
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// const API = process.env.REACT_APP_API_URL;
+
+// console.log(API);
+// function App() {
+//   const [days, setDays] = useState([]);
+//   useEffect(() => {
+//     axios
+//       .get(`${API}/test`)
+//       .then(
+//         (response) => {
+//           setDays(response.data);
+//         },
+//         (error) => console.log("get", error)
+//       )
+//       .catch((c) => console.warn("catch", c));
+//   }, []);
+//   return (
+//     <div>
+//       <ul>
+//         {days.map((day) => (
+//           <li key={day.name}>{day.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
