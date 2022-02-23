f713360ff2c656926a9ff6ded2cf9455ca6da88b
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-23 16:13:33 -0500

    Create Product Card

--- front-end/src/Components/Product.js
+++ front-end/src/Components/Product.js
@@ -1,26 +1,19 @@
 import { Link } from "react-router-dom";
 
 function Product({ product, id }) {
+  console.log('HELLO', product)
   return (
-    <div className="wrapper">
-      <div className="card">
-        <div>
-          <p>
-            <img src={product.image} alt="Product Thumbnail" />
-          </p>
-
-          <p>
+      <div className="productCard">
             <Link style={{ color: "#66A3A3" }} to={`/products/${id}`}>
               {product.name}
             </Link>
-          </p>
-          <p className="favorite">
-            {product.is_healthy ? <span>♥</span> : <span>♡</span>}
+        
+          <p className="featured">
+            {product.featured ? <span>♥</span> : <span>♡</span>}
           </p>
         </div>
-      </div>
-    </div>
-  );
+  
+  ); 
 }
 
 export default Product;
